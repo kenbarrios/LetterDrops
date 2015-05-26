58cfd31930e6e968f9605ab081ff1bcb9132b3b6
Ken Barrios <keniswaiting@gmail.com>
2015-05-25 21:53:00 -0500

    removes toggle for css visibility hidden to keep straight lines

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,108 +4,107 @@ $(document).ready(function(){
 
      $(document).keypress(function(event){
       if(event.keyCode == 97){
-        $('.divA').toggle("explode");
-
+        $('.divA').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 98){
-        $('.divB').toggle("explode");
+        $('.divB').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 99){
-        $('.divC').toggle("explode");
+        $('.divC').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 100){
-        $('.divD').toggle("explode");
+        $('.divD').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 101){
-        $('.divE').toggle("explode");
+        $('.divE').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 102){
-        $('.divF').toggle("explode");
+        $('.divF').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 103){
-        $('.divG').toggle("explode");
+        $('.divG').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 104){
-        $('.divH').toggle("explode");
+        $('.divH').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 105){
-        $('.divI').toggle("explode");
+        $('.divI').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 106){
-        $('.divJ').toggle("explode");
+        $('.divJ').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 107){
-        $('.divK').toggle("explode");
+        $('.divK').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 108){
-        $('.divL').toggle("explode");
+        $('.divL').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 109){
-        $('.divM').toggle("explode");
+        $('.divM').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 110){
-        $('.divN').toggle("explode");
+        $('.divN').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 111){
-        $('.divO').toggle("explode");
+        $('.divO').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 112){
-        $('.divP').toggle("explode");
+        $('.divP').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 113){
-        $('.divQ').toggle("explode");
+        $('.divQ').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 114){
-        $('.divR').toggle("explode");
+        $('.divR').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 115){
-        $('.divS').toggle("explode");
+        $('.divS').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 116){
-        $('.divT').toggle("explode");
+        $('.divT').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 117){
-        $('.divU').toggle("explode");
+        $('.divU').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 118){
-        $('.divV').toggle("explode");
+        $('.divV').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 119){
-        $('.divW').toggle("explode");
+        $('.divW').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 120){
-        $('.divX').toggle("explode");
+        $('.divX').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 121){
-        $('.divY').toggle("explode");
+        $('.divY').css("visibility", "hidden");
       }
 
       else if(event.keyCode == 122){
-        $('.divZ').toggle("explode");
+        $('.divZ').css("visibility", "hidden");
       }
 
     }); //closes document.keypress
