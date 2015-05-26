31028115c1686cb7995195b1286acd333c2e35e9
Ken Barrios <keniswaiting@gmail.com>
2015-05-25 21:10:42 -0500

    adds js files, full alphabet, and explode fntn

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,118 @@
+$(document).ready(function(){
+
+  function popDroplet(){
+
+     $(document).keypress(function(event){
+      if(event.keyCode == 97){
+        $('.divA').toggle("explode");
+
+      }
+
+      else if(event.keyCode == 98){
+        $('.divB').toggle("explode");
+      }
+
+      else if(event.keyCode == 99){
+        $('.divC').toggle("explode");
+      }
+
+      else if(event.keyCode == 100){
+        $('.divD').toggle("explode");
+      }
+
+      else if(event.keyCode == 101){
+        $('.divE').toggle("explode");
+      }
+
+      else if(event.keyCode == 102){
+        $('.divF').toggle("explode");
+      }
+
+      else if(event.keyCode == 103){
+        $('.divG').toggle("explode");
+      }
+
+      else if(event.keyCode == 104){
+        $('.divH').toggle("explode");
+      }
+
+      else if(event.keyCode == 105){
+        $('.divI').toggle("explode");
+      }
+
+      else if(event.keyCode == 106){
+        $('.divJ').toggle("explode");
+      }
+
+      else if(event.keyCode == 107){
+        $('.divK').toggle("explode");
+      }
+
+      else if(event.keyCode == 108){
+        $('.divL').toggle("explode");
+      }
+
+      else if(event.keyCode == 109){
+        $('.divM').toggle("explode");
+      }
+
+      else if(event.keyCode == 110){
+        $('.divN').toggle("explode");
+      }
+
+      else if(event.keyCode == 111){
+        $('.divO').toggle("explode");
+      }
+
+      else if(event.keyCode == 112){
+        $('.divP').toggle("explode");
+      }
+
+      else if(event.keyCode == 113){
+        $('.divQ').toggle("explode");
+      }
+
+      else if(event.keyCode == 114){
+        $('.divR').toggle("explode");
+      }
+
+      else if(event.keyCode == 115){
+        $('.divS').toggle("explode");
+      }
+
+      else if(event.keyCode == 116){
+        $('.divT').toggle("explode");
+      }
+
+      else if(event.keyCode == 117){
+        $('.divU').toggle("explode");
+      }
+
+      else if(event.keyCode == 118){
+        $('.divV').toggle("explode");
+      }
+
+      else if(event.keyCode == 119){
+        $('.divW').toggle("explode");
+      }
+
+      else if(event.keyCode == 120){
+        $('.divX').toggle("explode");
+      }
+
+      else if(event.keyCode == 121){
+        $('.divY').toggle("explode");
+      }
+
+      else if(event.keyCode == 122){
+        $('.divZ').toggle("explode");
+      }
+
+    }); //closes document.keypress
+
+    } //closes popDroplet()
+
+      popDroplet();
+
+
+ }); // closes document.ready
