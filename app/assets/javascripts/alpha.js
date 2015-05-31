d9a84f6b08b4361b9432ccdd31cd9c3c0a6a53fc
Ken Barrios <keniswaiting@gmail.com>
2015-05-31 00:53:35 -0500

    adds consolidation to nav bar

--- app/assets/javascripts/alpha.js
+++ app/assets/javascripts/alpha.js
@@ -3,9 +3,19 @@ var app = app || {};
 
 app.popAlphaDroplet = function popDroplet(){
 
+  var keyList = [];
+
   $(document).keypress(function(event){
     var letter = String.fromCharCode(event.keyCode).toUpperCase();
     $('.div'+letter).css("visibility", "hidden");
   }); //closes document.keypress
+
+  keyList.push(String.fromCharCode(event.keyCode).toLowerCase());
+
+  function bubbleTracker(){
+    $('.liveText').text(keyList);
+  };// closes bubbleTracker
+
+  bubbleTracker();
 
 } //closes popAlphaDroplet()
